a71860f194a657b11287f178e67bfc149e5c2fce
itsax404 <quentin.davin@laposte.net>
2023-01-14 13:14:12 +0100

    Adding mercato function, removing the old timemanager and modifying team and time_manager classes
    In the main, I add mercato function and the start of time
    In the time_manager, I add a function to skip dates into mercato time (because I make all drafts in the same "day" and in mercato time, no matches was played so, skip that)
    In the championship, I add a function to get id of the object
    
    In the team, I add two functions: the first is for get all players who players in specific post
    the second is to get the amount of players in the team

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ from generate_players import generate_players
 from team import Team
 from country import Country
 from championship import Championship
+import random
 
+def mercato(championship_list, players_number: int, teams_per_championship):
+    """
+    This function permits to "recreate" a mercato.
+    """
+    numbers_players_to_draft = round(players_number*0.035) # Get the approximatly number of players to need drafts
+    for x in numbers_players_to_draft:
+        championships = random.choices(championship_list, k=2) # I get two randoms championships
+        teams = list() # I create a list for add two randoms teams
+        for championship in championships:
+            id_champ = championship.get_ip # I get the id of the championship
+            teams.append(teams_per_championship[id_champ][random.choice(teams_per_championship[id_champ])]) # I add a random team in this championship
+        player1 = random.choice(teams[0].get_players()) # I choose a random player in this team
+        player2 = random.choice(teams[1].get_players_per_post(player1.get_post())) # I choose a player with the SAME post in the other team
+        teams[0].remove(player1) # I switch players into teams
+        teams[1].remove(player2)
+        teams[1].add(player1)
+        teams[0].add(player2)
+        # TODO contracts
 
 def main():
     nationalities_countries_dict = load_csv("nationalites_pays")
@@ -13,15 +32,25 @@ def main():
         nationalities_countries.append(Country(nc["id_pays"], nc["id_nationalite"]))
     championships_dict = load_csv("championnats")
     championships = []
+    teams_per_championships = {}
     for championship in championships_dict:
         championships.append(Championship(championship["id_championnat"], championship["id_pays"], nationalities_countries))
+        teams_per_championships[championship["id_championnat"]] = list()
     teams_dict = load_csv("teams")
     teams = []
-    for team in teams_dict:
+    for i, team in enumerate(teams_dict):
         teams.append(Team(team["id_club"], team["id_championnat"], championships))
+        teams_per_championships[team["id_championnat"]].append(i)
+    players_number = 0
     for team in teams:
         generate_players(team)
+        players_number += team.get_amount_players()
     time_manager = TimeManager()
+    if (time_manager.mercato_time):
+        mercato(championships, players_number, teams_per_championships)
+        time_manager.skip_mercato_time()
+    else:
+        time_manager.add_day()
     return  # nb de joueurs par équipe : 22-25 pour EN et ES, 22-36 sinon
 
 
--- time_manager.py
+++ time_manager.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from datetime import timedelta
 
 class TimeManager:
     def __init__(self, day: int = 8, month: int = 1, year: int = 1970) -> None:
@@ -12,6 +12,18 @@ class TimeManager:
 
     def match_time(self) -> bool:
         return not (self.mercato_time())
+
+    def skip_mercato_time(self):
+        if self.match_time():
+            return
+        current_year = self.date.year
+        if(self.date.month == 12 or self.date.month == 1):
+            self.date = datetime(8,  1, current_year+1)
+        else:
+            self.date = datetime(31, 8, current_year)
+
+    def add_day(self):
+        self.date += timedelta(days=1)
 
     def __str__(self) -> str:  # French way of displaying date
         return self.date.strftime("%d/%m/%Y")
