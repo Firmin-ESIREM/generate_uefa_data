31320062958e1864114d4ab9bb35a7a49fdcadd8
Firmin Launay <hey@firminlaunay.me>
2023-01-15 20:52:59 +0100

    merge

--- generate_players.py
+++ generate_players.py
@@ -1,4 +1,11 @@
 from team import Team
+from random import randint
+
 
 def generate_players(team: Team) -> list:
+    if team.championship.country.id_country == '155' or team.championship.country.id_country == '55':
+        number_of_players = randint(22, 25)
+    else:
+        number_of_players = randint(22, 36)
+    #
     return []
--- team.py
+++ team.py
@@ -4,11 +4,10 @@ from player import Player
 
 class Team:
     def __init__(self, id_team: str, id_championship: str, championships: list[Championship]) -> None:
-        print(championships)
         self.id_team: str = id_team
         self.championship: Championship = next((x for x in championships if x.id_championship == id_championship), None)
-        self.players = []
-        self.players_per_post = {"A": [], "M": [], "D": [], "G": []}
+        self.players: list[Player] = []
+        self.players_per_post: dict[str, list[Player]] = {"A": [], "M": [], "D": [], "G": []}
 
     def add_player(self, player: Player):
         if player not in self.players:
